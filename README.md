022e3e7ab0780fb11fe8b3dfad82bb1d91cb6312
YangBo <tallonink@gmail.com>
2023-04-22 01:03:12 +0800

    feat:amazon-oss

---
+++ README.md
@@ -0,0 +1,24 @@
+# amazon-oss-stater
+
+一款开箱即用的 Spring Boot Stater 企业级 OSS 对象存储服务
+
+<br/>
+适配 阿里云OSS、腾讯COS、七牛云OSS、MInio 等
+
+<br/>
+<br/>
+
+[Amazon Simple Storage Service](https://docs.aws.amazon.com/zh_cn/AmazonS3/latest/userguide/Welcome.html )（Amazon S3，Amazon 简便存储服务）是 AWS 最早推出的云服务之一，经过多年的发展，S3 协议在对象存储行业事实上已经成为标准。
+
+1. 提供了统一的接口 REST/SOAP 来统一访问任何数据
+2. 对 S3 来说，存在里面的数据就是对象名（键），和数据（值）
+3. 不限量，单个文件最高可达 5TB，可动态扩容。
+3. 高速。每个 bucket 下每秒可达 3500 PUT/COPY/POST/DELETE 或 5500 GET/HEAD 请求。
+4. 具备版本，权限控制能力
+5. 具备数据生命周期管理能力
+
+<br/>
+<br/>
+为什么要基于AmazonS3 实现 Spring Boot Starter ？
+<br/>
+市面上 OSS 对象存储服务基本都支持 AmazonS3，封装自己的 starter 那么就必须考虑适配，迁移，可扩展。比如现在使用的是阿里云 OSS 对接阿里云 OSS 的 SDK，之后使用的是腾讯 COS 对接是腾讯云 COS，那么直接对接 AmazonS3 实现，这样后续不需要调整代码，只需要去各个云服务商配置即可
--- src/main/java/com/oss/core/OssTemplate.java
+++ src/main/java/com/oss/core/OssTemplate.java
@@ -1,2 +1,85 @@
-package com.oss.core;public class OssTemplate {
+package com.oss.core;
+
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * OSS 操作模板
+ */
+public interface OssTemplate {
+
+    /**
+     * 创建bucket
+     * @param bucketName bucket名称
+     */
+    void createBucket(String bucketName);
+
+    /**
+     * 获取所有的bucket
+     * @return bucket
+     */
+    List<Bucket> getAllBuckets();
+
+    /**
+     * 通过bucket名称删除bucket
+     * @param bucketName name
+     */
+    void removeBucket(String bucketName);
+
+    /**
+     * 上传文件
+     * @param bucketName bucket名称
+     * @param objectName 文件名称
+     * @param stream 文件流
+     * @param contextType 文件类型
+     * @throws Exception exception
+     */
+    void putObject(String bucketName, String objectName, InputStream stream, String contextType) throws Exception;
+
+    /**
+     * 上传文件
+     * @param bucketName bucket名称
+     * @param objectName 文件名称
+     * @param stream 文件流
+     * @throws Exception exception
+     */
+    void putObject(String bucketName, String objectName, InputStream stream) throws Exception;
+
+    /**
+     * 获取文件
+     * @param bucketName bucket名称
+     * @param objectName 文件名称
+     * @return S3Object
+     */
+    S3Object getObject(String bucketName, String objectName);
+
+    /**
+     * 获取对象的url
+     * @param bucketName name
+     * @param objectName name
+     * @param expires expires
+     * @return 对象URL
+     */
+    String getObjectURL(String bucketName, String objectName, Integer expires);
+
+    /**
+     * 通过bucketName和objectName删除对象
+     * @param bucketName name
+     * @param objectName name
+     * @throws Exception exception
+     */
+    void removeObject(String bucketName, String objectName) throws Exception;
+
+    /**
+     * 根据文件前置查询文件
+     * @param bucketName bucket名称
+     * @param prefix 前缀
+     * @param recursive 是否递归查询
+     * @return S3ObjectSummary 列表
+     */
+    List<S3ObjectSummary> getAllObjectsByPrefix(String bucketName, String prefix, boolean recursive);
 }
